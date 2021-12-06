7e5e3a13092149c836d8040560a833b87219605b
woogachaka <woogachaka@nate.com>
2021-12-06 18:05:15 +0900

    커밋

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { NumberValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: `
+    <ht>{{title}}</ht>
+    <h2>{{hero.name}}</h2>
+    <div><label>id : </label>{{hero.id}}</div>
+    <div>
+      <label>이름 : </label>
+      <input [(ngModel)]="hero.name" placeholder="이름"/>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+
+  // title = "Tour of Heroes";
+
+  // hero : Hero = {
+  //   id : 1,
+  //   name : "이순신"
+  // }
 }
+
+// export class Hero {
+//   id : NumberValueAccessor;``
+//   name : string;
+// }
