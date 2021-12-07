d34ae24f58e1e314d66077c1d378b069cd010ed1
woogachaka <woogachaka@nate.com>
2021-12-07 11:36:04 +0900

    작업중

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,10 @@
 import { Component } from '@angular/core';
-import { NumberValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: `
-    <ht>{{title}}</ht>
-    <h2>{{hero.name}}</h2>
-    <div><label>id : </label>{{hero.id}}</div>
-    <div>
-      <label>이름 : </label>
-      <input [(ngModel)]="hero.name" placeholder="이름"/>
-    </div><div>test</div>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  // title = "Tour of Heroes";
-
-  // hero : Hero = {
-  //   id : 1,
-  //   name : "이순신"
-  // }
+  title = 'angular-tour-of-heroes';
 }
-
-// export class Hero {
-//   id : NumberValueAccessor;``
-//   name : string;
-// }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Hero } from '../Hero';
+// import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  // hero : Hero = {
+  //   id : 1,
+  //   name : 'Windostorm'
+  // };
+
+  heroes : Hero[] = [];
+
+  constructor(private heroService : HeroService, private messageService : MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
+  selectedHero? : Hero;
+
+  onSelect(hero : Hero) : void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroComponent : Selected Hero id =${hero.id}`);
+  }
+
+  getHeroes() : void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 }
